ed8d2dc70aa71bf6e66cf41347c94548b9974cf2
Jomerjom <ekenesjoachim@gmail.com>
2024-01-05 13:50:30 +0100

    swapped from picture to figure, added 2 rem padding on content

--- src/components/perfumeImage/PerfumeImage.jsx
+++ src/components/perfumeImage/PerfumeImage.jsx
@@ -4,10 +4,10 @@ import './PerfumeImage.css'
 
 export const PerfumeImage = () => {
     return (
-        <picture className="img_wrapper">
+        <figure className="img_wrapper">
         {/* //TODO: Bruk media queries for å vise riktig bilde (1440px for desktop, 375px for mobile) */}
         <img src={BGDESKTOP} alt="" className="desktop_image" />
         <img src={BGMOBILE} alt="" className="mobile_image" />
-      </picture>
+      </figure>
     )
 }
